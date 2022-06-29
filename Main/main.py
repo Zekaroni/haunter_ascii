551cd67b25989c0a668a40a923c4e308444d53c8
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2022-06-29 08:54:02 -0400

    not wanting to save

--- Main/main.py
+++ Main/main.py
@@ -1,6 +1,7 @@
 from blessings import Terminal
 import time
 from ascii_mod import *
+import subprocess as sp
 
 # Starts with a countdown 
 for i in range(5):
@@ -16,16 +17,21 @@ while True:
     width, height = [t.width,t.height]
     screenshot()
     with t.location(0, 0):
-        print(Ascii(f'./screenshots/Pokemon-0.png',braille=True,threshold=150,dimensions=f'{width - round(width*0.25)},{height - round(height*0.1)}'))
+        try:
+            print(Ascii(f'./screenshots/Pokemon-0.png',color=True,braille=True,threshold=130,dimensions=f'{width - round(width*0.25)},{height - round(height*0.1)}'))
+        except Exception:
+            print('Waiting 3 seconds due to error')
+            time.sleep(3)
+            clear()
     time.sleep(0.04)
     try:
-        os.system('rm ./screenshots/Pokemon-0.png')
+        sp.run(['rm ./screenshots/Pokemon-0.png'], check = True)
     except Exception:
-        for i in range(200):
-            try:
-                os.system(f'rm ./screenshots/Pokemon-{i+1}.png')
-            except Exception:
-                break
+        files = sp.getoutput('find ./screenshots -type f -name "*.png"')
+        files = files.split('\n')
+        for file in files:
+            os.system(f'rm {file}')
+        
 end = time.time()
 
 print(f'Rendered at {1200/(end-start)} frames per second')
